e44f212b561e8e778bb4d911804556a04eb71655
mrdbrg <rdg.brg@gmail.com>
2023-08-04 19:47:42 -0500

    display beans from application context / execute custom bean configuration example

--- src/main/java/com/havefunwith/Main.java
+++ src/main/java/com/havefunwith/Main.java
@@ -1,7 +1,11 @@
 package com.havefunwith;
 
+import com.havefunwith.singleton.Food;
+import com.havefunwith.singleton.FoodService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,110 +14,38 @@ import java.util.Objects;
 
 
 @SpringBootApplication
-@RestController
 public class Main {
 
-    private static List<Customer> customers;
+    private static FoodService foodService = null;
 
-    static {
-        customers = new ArrayList<>();
-
-        Customer john = new Customer(1, "John", 24, "john@email.com");
-        Customer keyla = new Customer(2, "Keyla", 28, "keila@email.com");
-
-        customers.add(john);
-        customers.add(keyla);
+    public Main(FoodService foodService) {
+        this.foodService = foodService;
     }
 
     public static void main(String[] args) {
-//        System.out.println("Customers DB :: " + customers);
-        SpringApplication.run(Main.class, args);
+        /*
+            Never instantiate classes like this:
+            CustomerService customerService = new CustomerService(new CustomerDataAccessService());
+            CustomerController customerController = new CustomerController(customerService);
+         */
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(Main.class, args);
+
+        // execute bean
+        foodService.printFoodDetails();
+
+        printBeans(applicationContext);
+
     }
 
-//    @RequestMapping(value = "/api/v1/customers", method = RequestMethod.GET)
-    @GetMapping("/api/v1/customers")
-    public List<Customer> getCustomers() {
-        return customers;
-    }
+    private static void printBeans(ConfigurableApplicationContext cntx) {
 
-    @GetMapping("/api/v1/customers/{customerId}")
-    public Customer getCustomer(@PathVariable Integer customerId) {
-        return customers.stream()
-                .filter(customer -> customer.id.equals(customerId))
-                .findFirst()
-                .orElseThrow(
-                        () -> new IllegalArgumentException("Customer with id [%s] not found.".formatted(customerId)));
-    }
+        String[] beanDefinitionNames = cntx.getBeanDefinitionNames();
 
-    static class Customer {
-        private Integer id;
-        private String name;
-        private Integer age;
-        private String email;
-
-        public Customer() {}
-
-        public Customer(Integer id, String name, Integer age, String email) {
-            this.id = id;
-            this.name = name;
-            this.age = age;
-            this.email = email;
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
         }
 
-        public Integer getId() {
-            return id;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Integer getAge() {
-            return age;
-        }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Customer customer = (Customer) o;
-            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(age, customer.age) && Objects.equals(email, customer.email);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name, age, email);
-        }
-
-        @Override
-        public String toString() {
-            return "Customer{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", age=" + age +
-                    ", email='" + email + '\'' +
-                    '}';
-        }
     }
 
 }
