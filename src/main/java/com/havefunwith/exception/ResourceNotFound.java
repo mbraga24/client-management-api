f0badd3f72f96bf3caafbf67bc08ea30ed906e2e
mrdbrg <rdg.brg@gmail.com>
2023-08-06 11:20:43 -0500

    custom exception

---
+++ src/main/java/com/havefunwith/exception/ResourceNotFound.java
@@ -0,0 +1,13 @@
+package com.havefunwith.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFound extends RuntimeException {
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+
+}
