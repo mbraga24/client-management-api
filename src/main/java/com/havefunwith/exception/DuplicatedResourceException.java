eb4d845d51991577c1832c99bd04bd4c5ba8f1dc
mrdbrg <rdg.brg@gmail.com>
2023-09-05 17:57:06 -0500

    add DuplicatedResourceException

---
+++ src/main/java/com/havefunwith/exception/DuplicatedResourceException.java
@@ -0,0 +1,13 @@
+package com.havefunwith.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DuplicatedResourceException extends RuntimeException {
+
+    public DuplicatedResourceException(String message) {
+        super(message);
+    }
+
+}
