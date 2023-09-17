881a94da4dbafd2c18103f94862b795f1f2fc2ec
mrdbrg <rdg.brg@gmail.com>
2023-09-17 16:33:29 -0500

    set up jib

--- src/test/java/com/havefunwith/customer/integration/CustomerIntegrationTest.java
+++ src/test/java/com/havefunwith/customer/integration/CustomerIntegrationTest.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CustomerControllerTest {
+public class CustomerIT {
 
     @Autowired
     private WebTestClient webTestClient;
