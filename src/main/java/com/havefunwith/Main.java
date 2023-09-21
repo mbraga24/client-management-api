3607cdaf1df01b3deede69b08e4715cd0f447273
mrdbrg <rdg.brg@gmail.com>
2023-09-21 17:02:29 -0500

    main class clean up

--- src/main/java/com/havefunwith/Main.java
+++ src/main/java/com/havefunwith/Main.java
@@ -14,23 +14,8 @@ import java.util.Random;
 @SpringBootApplication
 public class Main {
 
-//    private final CustomerService customerService;
-
-//    public Main(CustomerService customerService) {
-//        this.customerService = customerService;
-//    }
-
     public static void main(String[] args) {
-        /*
-            Never instantiate classes like this:
-            CustomerService customerService = new CustomerService(new CustomerDataAccessService());
-            CustomerController customerController = new CustomerController(customerService);
-         */
-//        ConfigurableApplicationContext applicationContext =
-                SpringApplication.run(Main.class, args);
-
-        //  printBeans(applicationContext);
-
+        SpringApplication.run(Main.class, args);
     }
 
     @Bean
@@ -49,28 +34,4 @@ public class Main {
             customerRepository.save(customer);
         };
     }
-    /*
-    @Component
-    public static class CustomBeanRunner implements CommandLineRunner {
-        private final ProductService productService;
-
-        public CustomBeanRunner(ProductService productService) {
-            this.productService = productService;
-        }
-
-        // "String...": Special syntax called "varargs" (variable-length argument list).
-        // It allows the method to accept an arbitrary number of String arguments,
-        // including zero arguments.
-        public void run(String... args) {
-            productService.processProduct();
-        }
-    }
-
-    private static void printBeans(ConfigurableApplicationContext cntx) {
-        String[] beanDefinitionNames = cntx.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
-    }
-    */
 }
