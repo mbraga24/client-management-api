31ebc8208f858b55230daae56823a7beb626c6bb
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-10-11 08:05:57 -0500

    CD workflow setup with new controller

--- Backend/src/main/java/com/havefunwith/CloudController.java
+++ Backend/src/main/java/com/havefunwith/CloudController.java
@@ -5,12 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CloudController {
-
     record Cloud(boolean inTheSky) {}
-
     @GetMapping("/cloud")
     public Cloud getCloud() {
         return new Cloud(true);
     }
-
 }
