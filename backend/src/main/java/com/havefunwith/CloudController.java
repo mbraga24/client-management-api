a1840efe02a0a1cdac9a657a4bcdd66815afaf63
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-10-24 08:17:20 -0500

    Feature/devops ci cd (#30)
    ci/cd - test

---
+++ backend/src/main/java/com/havefunwith/CloudController.java
@@ -0,0 +1,13 @@
+package com.havefunwith;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CloudController {
+    record Cloud(boolean inTheSky) {}
+    @GetMapping("/cloud")
+    public Cloud getCloud() {
+        return new Cloud(true);
+    }
+}
