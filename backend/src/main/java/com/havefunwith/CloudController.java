94f2d932fba82f9b6044152178520aea7fab9fed
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-10-26 08:14:17 -0500

    Feature/devops ci cd (#38)
    * ci/cd test - fix

--- backend/src/main/java/com/havefunwith/CloudController.java
+++ backend/src/main/java/com/havefunwith/CloudController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CloudController {
     record Cloud(boolean inTheSky) {}
-    // Change
+    // Test CI/CD
     @GetMapping("/cloud")
     public Cloud getCloud() {
         return new Cloud(true);
