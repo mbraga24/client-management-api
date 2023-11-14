fee0c39004ad58612d67525bb4afb804dda159a0
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-11-14 07:53:17 -0600

    test ci/cd with slack integration

--- backend/src/main/java/com/havefunwith/CloudController.java
+++ backend/src/main/java/com/havefunwith/CloudController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CloudController {
     record Cloud(boolean inTheSky) {}
-
+    // ==================================
     // Testing CI/CD - Slack integration
+    // ==================================
     @GetMapping("/cloud")
     public Cloud getCloud() {
         return new Cloud(true);
