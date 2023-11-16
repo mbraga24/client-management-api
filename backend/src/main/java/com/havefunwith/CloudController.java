e679710430efd2f25b98d19fffb2e9479fb59aa2
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-11-16 16:21:58 -0600

    Feature/ci cd-4:14:11:16 (#55)
    slack integration-4:14:11:16

--- backend/src/main/java/com/havefunwith/CloudController.java
+++ backend/src/main/java/com/havefunwith/CloudController.java
@@ -3,9 +3,6 @@ package com.havefunwith;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
-    @RestController
- */
 @RestController
 public class CloudController {
     record Cloud(boolean inTheSky) {}
