9e7f03765abbe5234aaac3f2716e9c438f489a0a
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2023-11-16 15:11:49 -0600

    Feature/ci cd-3:08:11:16 (#52)
    slack integration-2:56:11:16

--- backend/src/main/java/com/havefunwith/CloudController.java
+++ backend/src/main/java/com/havefunwith/CloudController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
-    @RestController
+    @RestController - Adding comment for testing
  */
 @RestController
 public class CloudController {
